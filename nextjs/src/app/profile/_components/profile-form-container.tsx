3b2f46d807675759ede5a32dfaabe22b8a7e6c47
sh-okada <sh-okada@enecom.co.jp>
2025-06-10 10:11:09 +0900

    リファクタリング

---
+++ nextjs/src/app/profile/_components/profile-form-container.tsx
@@ -0,0 +1,29 @@
+import { getDepartments } from "@/api/department/department";
+import { getGrades } from "@/api/grade/grade";
+import { ProfileForm } from "@/app/profile/_components/profile-form";
+
+export const ProfileFormContainer = async () => {
+	const grades = await getGrades();
+	const departments = await getDepartments();
+
+	const yearItems = Array.from({ length: 40 }, (_, i) => ({
+		value: (i + 1).toString(),
+		label: `${i + 1}年目`,
+	}));
+	const departmentItems = departments.data.map((department) => ({
+		value: department.id.toString(),
+		label: department.name,
+	}));
+	const gradeItems = grades.data.map((grade) => ({
+		value: grade.id.toString(),
+		label: grade.name,
+	}));
+
+	return (
+		<ProfileForm
+			yearItems={yearItems}
+			departmentItems={departmentItems}
+			gradeItems={gradeItems}
+		/>
+	);
+};
--- nextjs/src/app/profile/page.tsx
+++ nextjs/src/app/profile/page.tsx
@@ -1,38 +1,11 @@
-import { getDepartments } from "@/api/department/department";
-import { getGrades } from "@/api/grade/grade";
-import { ProfileForm } from "@/app/profile/_components/profile-form";
+import { ProfileFormContainer } from "@/app/profile/_components/profile-form-container";
+import { Spinner } from "@/components/ui-parts/spinner";
 import { Suspense } from "react";
 
-export default async function Page() {
-	const grades = await getGrades();
-	const departments = await getDepartments();
-
-	const yearItems = Array.from({ length: 40 }, (_, i) => ({
-		value: (i + 1).toString(),
-		label: `${i + 1}年目`,
-	}));
-	const departmentItems = departments.data.map((department) => ({
-		value: department.id.toString(),
-		label: department.name,
-	}));
-	const gradeItems = grades.data.map((grade) => ({
-		value: grade.id.toString(),
-		label: grade.name,
-	}));
-
+export default function Page() {
 	return (
-		<Suspense
-			fallback={
-				<div className="flex justify-center" aria-label="読み込み中">
-					<div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent" />
-				</div>
-			}
-		>
-			<ProfileForm
-				yearItems={yearItems}
-				departmentItems={departmentItems}
-				gradeItems={gradeItems}
-			/>
+		<Suspense fallback={<Spinner />}>
+			<ProfileFormContainer />
 		</Suspense>
 	);
 }
