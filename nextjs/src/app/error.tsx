b3ca37eebc433dd072129f0ba6398d5182a87373
sh-okada <sh-okada@ymail.ne.jp>
2025-06-10 15:35:17 +0000

    リファクタリング

--- nextjs/src/app/error.tsx
+++ nextjs/src/app/error.tsx
@@ -1,9 +1,5 @@
 "use client";
 
 export default function ErrorPage() {
-	return (
-		<div>
-			<h1 className="text-center">予期しないエラーが発生しました</h1>
-		</div>
-	);
+	return <h1 className="text-center">予期しないエラーが発生しました</h1>;
 }
