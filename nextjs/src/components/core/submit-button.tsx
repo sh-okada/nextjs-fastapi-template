bddcd05f0c14e22c013c6c36a2a0e35c6ec7a82e
sh-okada <sh-okada@ymail.ne.jp>
2025-06-10 13:55:09 +0000

    Buttonコンポーネントを使用するようにした

--- nextjs/src/components/core/submit-button.tsx
+++ nextjs/src/components/core/submit-button.tsx
@@ -1,7 +1,8 @@
+import type { ButtonProps } from "@/components/core/button";
 import type { ComponentProps } from "react";
 
-export type SubmitButtonProps = ComponentProps<"button">;
+export type SubmitButtonProps = Omit<ButtonProps, "type">;
 
-export const SubmitButton = ({ ...props }: SubmitButtonProps) => {
+export const SubmitButton = (props: SubmitButtonProps) => {
 	return <button type="submit" {...props} />;
 };
