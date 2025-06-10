5d8cd77674a82a994e366f15a8ddc2931067e467
sh-okada <sh-okada@ymail.ne.jp>
2025-06-10 15:40:24 +0000

    リファクタリング

--- nextjs/src/app/error.tsx
+++ nextjs/src/app/error.tsx
@@ -1,5 +1,23 @@
 "use client";
 
-export default function ErrorPage() {
-	return <h1 className="text-center">予期しないエラーが発生しました</h1>;
+import { Button } from "@/components/core/button";
+import { useEffect } from "react";
+
+export default function ErrorPage({
+	error,
+	reset,
+}: {
+	error: Error & { digest?: string };
+	reset: () => void;
+}) {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
+	return (
+		<div className="text-center">
+			<h2>予期しないエラーが発生しました。</h2>
+			<Button onClick={() => reset()}>もう一度試す</Button>
+		</div>
+	);
 }
