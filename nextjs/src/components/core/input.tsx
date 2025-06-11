e9864572e4442698c10e1cf3173fece35786ea7c
sh-okada <sh-okada@enecom.co.jp>
2025-06-11 01:00:38 +0000

    スタイル追加

--- nextjs/src/components/core/input.tsx
+++ nextjs/src/components/core/input.tsx
@@ -1,7 +1,19 @@
 import type { ComponentProps } from "react";
 
-export type InputProps = ComponentProps<"input">;
+const inputBaseStyle = `
+	border
+	border-black
+	rounded-md
+	px-4 py-3
+	focus-visible:outline focus-visible:outline-4 focus-visible:outline-black focus-visible:outline-offset-2
+	disabled:pointer-events-none disabled:text-gray-500 disabled:bg-gray-300 disabled:border-none
+	aria-[invalid=true]:border-red-900
+`;
 
-export const Input = (props: InputProps) => {
-	return <input {...props} />;
+export type InputProps = ComponentProps<"input"> & {
+	isError?: boolean;
+};
+
+export const Input = ({ isError, className = "", ...rest }: InputProps) => {
+	return <input className={`${inputBaseStyle} ${className}`} {...rest} />;
 };
