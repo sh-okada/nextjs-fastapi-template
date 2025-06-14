8f72ee6ecce8fd1cd898c4fcbc9bd53aa5c30ed4
sh-okada <sh-okada@ymail.ne.jp>
2025-06-14 01:52:15 +0000

    リファクタリング

--- nextjs/src/app/(private)/page.tsx
+++ nextjs/src/app/(private)/page.tsx
@@ -1,34 +1,19 @@
-import { Checkbox } from "@/components/core/checkbox";
 import { Markdown } from "@/components/ui-parts/markdown/markdown";
 import { PageContent } from "@/components/ui-parts/page-content";
 import { paths } from "@/config/paths";
 
 const markdownText = `
-# H1
-## H2
-### H3
-#### H4
-##### H5
-###### H6
-
-[リンクテキスト](http://github.com)
-
-- リスト1
-  - リスト1
-  - リスト2
-- リスト2
-  - リスト1
-
-1. リスト1
-  1. リスト1
-  1. リスト2
-1. リスト2
-1. リスト3
-
-~~~typescript
-// コード
-console.log("Hello, world!");
-~~~
+1. はじめに
+このサンプルアプリ以下の機能を実装しています。
+    - ログイン
+    - ユーザー登録
+    - シンプルなフォーム
+    - ステップ形式のフォーム
+1. 技術スタック
+    - フロントエンド
+      - Next.js
+    - バックエンド
+      - FastAPI
 `;
 
 export default async function Page() {
