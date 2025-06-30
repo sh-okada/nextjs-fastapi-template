5d6878a100e63d251c12f817f5467acf3ae7c91e
sh-okada <sh-okada@ymail.ne.jp>
2025-06-30 22:21:47 +0900

    リファクタリング

--- nextjs/src/app/(private)/(without-header)/doc/[slug]/page.tsx
+++ nextjs/src/app/(private)/(without-header)/doc/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from "react";
 import { DocContainer } from "@/app/(private)/(without-header)/doc/[slug]/_components/doc-container";
-import { PageContent } from "@/components/ui-parts/page-content";
 import { Spinner } from "@/components/ui-parts/spinner";
-import { paths } from "@/config/paths";
 
 type PageProps = {
   params: Promise<{ slug: string }>;
@@ -12,10 +10,8 @@ export default async function Page({ params }: PageProps) {
   const { slug } = await params;
 
   return (
-    <PageContent title={paths.doc.name}>
-      <Suspense fallback={<Spinner />}>
-        <DocContainer id={slug} />
-      </Suspense>
-    </PageContent>
+    <Suspense fallback={<Spinner />}>
+      <DocContainer id={slug} />
+    </Suspense>
   );
 }
