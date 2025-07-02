7df0d0239026fe97cb998163f296f750d0ceb7a2
sh-okada <sh-okada@enecom.co.jp>
2025-07-02 07:01:24 +0000

    リファクタリング

--- nextjs/src/components/ui-parts/markdown-editor/hooks/useMarkdownEditor.ts
+++ nextjs/src/components/ui-parts/markdown-editor/hooks/useMarkdownEditor.ts
@@ -1,4 +1,12 @@
+import {
+  defaultKeymap,
+  history,
+  historyKeymap,
+  indentWithTab,
+} from "@codemirror/commands";
+import { indentUnit } from "@codemirror/language";
 import { EditorState } from "@codemirror/state";
+import { keymap, placeholder } from "@codemirror/view";
 import { EditorView } from "codemirror";
 import { useEffect, useRef, useState } from "react";
 
@@ -16,7 +24,41 @@ export const useMarkdownEditor = () => {
 
   useEffect(() => {
     if (!view && container) {
-      const state = EditorState.create();
+      const state = EditorState.create({
+        extensions: [
+          placeholder("Type your markdown here..."),
+          history(),
+          keymap.of(defaultKeymap),
+          keymap.of(historyKeymap),
+          keymap.of([indentWithTab]),
+          indentUnit.of("    "),
+          EditorView.theme({
+            "&": {
+              border: "1px solid #000000",
+              borderRadius: "0.375rem",
+              fontSize: "16px",
+            },
+            "&.cm-editor": {
+              outline: "none",
+            },
+            "&.cm-focused": {
+              outline: "4px solid #000000",
+              outlineOffset: "2px",
+            },
+            "&:focus-visible": {
+              outline: "4px solid #000000",
+              outlineOffset: "2px",
+            },
+            ".cm-content": {
+              padding: "12px 16px 12px 16px",
+              fontFamily: "Noto Sans JP",
+            },
+            ".cm-scroller": {
+              overflow: "auto",
+            },
+          }),
+        ],
+      });
       const viewCurrent = new EditorView({
         state,
         parent: container,
