e9549053500d9fdd7ceb113cc474b404db0414a9
sh-okada <sh-okada@enecom.co.jp>
2025-07-02 08:21:15 +0000

    リファクタリング

--- nextjs/src/components/ui-parts/markdown-editor/hooks/useMarkdownEditor.ts
+++ nextjs/src/components/ui-parts/markdown-editor/hooks/useMarkdownEditor.ts
@@ -1,16 +1,10 @@
-import {
-  defaultKeymap,
-  history,
-  historyKeymap,
-  indentWithTab,
-} from "@codemirror/commands";
-import { indentUnit } from "@codemirror/language";
-import { EditorState } from "@codemirror/state";
-import { keymap, placeholder } from "@codemirror/view";
+import { EditorState, type EditorStateConfig } from "@codemirror/state";
 import { EditorView } from "codemirror";
 import { useEffect, useRef, useState } from "react";
 
-export const useMarkdownEditor = () => {
+export type UseMarkdownEditorProps = EditorStateConfig;
+
+export const useMarkdownEditor = (props: UseMarkdownEditorProps) => {
   const editorRef = useRef(null);
   const [container, setContainer] = useState<HTMLDivElement>();
   const [view, setView] = useState<EditorView>();
@@ -24,48 +18,14 @@ export const useMarkdownEditor = () => {
 
   useEffect(() => {
     if (!view && container) {
-      const state = EditorState.create({
-        extensions: [
-          placeholder("Type your markdown here..."),
-          history(),
-          keymap.of(defaultKeymap),
-          keymap.of(historyKeymap),
-          keymap.of([indentWithTab]),
-          indentUnit.of("    "),
-          EditorView.theme({
-            "&": {
-              border: "1px solid #000000",
-              borderRadius: "0.375rem",
-              fontSize: "16px",
-            },
-            "&.cm-editor": {
-              outline: "none",
-            },
-            "&.cm-focused": {
-              outline: "4px solid #000000",
-              outlineOffset: "2px",
-            },
-            "&:focus-visible": {
-              outline: "4px solid #000000",
-              outlineOffset: "2px",
-            },
-            ".cm-content": {
-              padding: "12px 16px 12px 16px",
-              fontFamily: "Noto Sans JP",
-            },
-            ".cm-scroller": {
-              overflow: "auto",
-            },
-          }),
-        ],
-      });
+      const state = EditorState.create(props);
       const viewCurrent = new EditorView({
         state,
         parent: container,
       });
       setView(viewCurrent);
     }
-  }, [view, container]);
+  }, [view, container, props]);
 
   return {
     editorRef,
--- nextjs/src/components/ui-parts/markdown-editor/markdown-editor.tsx
+++ nextjs/src/components/ui-parts/markdown-editor/markdown-editor.tsx
@@ -1,9 +1,75 @@
 "use client";
 
 import { useMarkdownEditor } from "@/components/ui-parts/markdown-editor/hooks/useMarkdownEditor";
+import {
+  defaultKeymap,
+  history,
+  historyKeymap,
+  indentWithTab,
+} from "@codemirror/commands";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import {
+  HighlightStyle,
+  indentUnit,
+  syntaxHighlighting,
+} from "@codemirror/language";
+import { keymap, placeholder } from "@codemirror/view";
+import { tags } from "@lezer/highlight";
+import { EditorView } from "codemirror";
+
+// const highlightStyle = HighlightStyle.define([
+//   { tag: tags.heading1, color: "black", fontSize: "1.4em" },
+//   { tag: tags.heading2, color: "black", fontSize: "1.3em" },
+//   { tag: tags.heading3, color: "black", fontSize: "1.2em" },
+//   { tag: tags.heading4, color: "black", fontSize: "1.1em" },
+//   { tag: tags.strong, color: "black", fontWeight: "700" },
+//   { tag: tags.quote, color: "#6a737d" },
+//   { tag: tags.emphasis, fontStyle: "italic" },
+//   { tag: tags.url, textDecoration: "underline" },
+//   { tag: tags.strikethrough, textDecoration: "line-through" },
+// ]);
 
 export const MarkdownEditor = () => {
-  const { editorRef } = useMarkdownEditor();
+  const { editorRef } = useMarkdownEditor({
+    extensions: [
+      placeholder("Type your markdown here..."),
+      history(),
+      keymap.of(defaultKeymap),
+      keymap.of(historyKeymap),
+      keymap.of([indentWithTab]),
+      indentUnit.of("    "),
+      // syntaxHighlighting(highlightStyle),
+      markdown({
+        base: markdownLanguage,
+        completeHTMLTags: false,
+      }),
+      EditorView.theme({
+        "&": {
+          border: "1px solid #000000",
+          borderRadius: "0.375rem",
+          fontSize: "16px",
+        },
+        "&.cm-editor": {
+          outline: "none",
+        },
+        "&.cm-focused": {
+          outline: "4px solid #000000",
+          outlineOffset: "2px",
+        },
+        "&:focus-visible": {
+          outline: "4px solid #000000",
+          outlineOffset: "2px",
+        },
+        ".cm-content": {
+          padding: "12px 16px 12px 16px",
+          fontFamily: "Noto Sans JP",
+        },
+        ".cm-scroller": {
+          overflow: "auto",
+        },
+      }),
+    ],
+  });
 
   return <div ref={editorRef} />;
 };
